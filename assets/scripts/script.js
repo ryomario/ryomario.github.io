81f99ee3e19dc8d735070576c2499d7bf9548568
Mario <mario@ecampuz.com>
2024-02-17 16:40:40 +0700

    about-me:Add download link CV

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,8 +3,10 @@ import profile from '../datas/profile.json' assert { type: 'json' };
 const nameElements = document.querySelectorAll('[data-profile=name]');
 const jobsInlineElements = document.querySelectorAll('[data-profile=jobs-inline]');
 const jobsDescElements = document.querySelectorAll('[data-profile=jobs-desc]');
+const downloadCVAnchorElements = document.querySelectorAll('a[data-profile=download-cv]');
 
 nameElements.forEach(el => el.textContent = profile.name);
 jobsInlineElements.forEach(el => el.textContent = profile.jobs.join(', '))
 jobsDescElements.forEach(el => el.innerHTML = profile.jobsDesc.map(v => `<p>${v}</p>`).join(''));
+downloadCVAnchorElements.forEach(el => el.href = profile.downloadCV);
 // console.log(new Date(profile.born));
