c6a6dc5b0e613745b79556a7aad04007c6153e21
Mario <mario@ecampuz.com>
2024-02-18 18:20:34 +0700

    feat: Carousel at my work items + new data my works

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,9 +14,9 @@ downloadCVAnchorElements.forEach(el => el.href = profile.downloadCV);
 
 // My works
 const worksContainer = document.querySelector('[data-container=works]');
-const createWorkElement = (work) => {
+const createWorkElement = (work,active) => {
     const txt = `
-    <div class="col-md-4">
+    <div class="carousel-item ${active?'active':''}">
         <div class="feature-content">
             <img src="${work.imageSrc}" alt="Image">
             <h2 class="feature-content-title green-text">${work.name}</h2>
@@ -28,8 +28,47 @@ const createWorkElement = (work) => {
 }
 
 worksContainer.childNodes.forEach(node => node.remove());
+let active = true;
 works.forEach(work => {
-    const tmpl = createWorkElement(work);
+    const tmpl = createWorkElement(work,active);
+    active = false;
 
     worksContainer.appendChild(tmpl);
 });
+const myWorkCarouselEl = document.querySelector('#workCarousel');
+
+if (window.matchMedia("(min-width:640px)").matches){
+    (()=>{
+        const carousel = new bootstrap.Carousel(myWorkCarouselEl, {
+            interval: false
+        })
+
+        const carouselWidth = worksContainer.scrollWidth;
+        const workItemWidth = $('.carousel-item').width();
+        const visibleCarouselWidth = $('.carousel').width();
+    
+        let scrollPosition = 0;
+        $('.carousel-control-next').on('click', function(){
+            if(scrollPosition < (carouselWidth - visibleCarouselWidth)) {
+                scrollPosition = scrollPosition + workItemWidth;
+            } else {
+                scrollPosition = 0;
+            }
+            $('.carousel-inner').animate({scrollLeft: scrollPosition},600);
+        });
+    
+        $('.carousel-control-prev').on('click', function(){
+            if(scrollPosition > 0) {
+                scrollPosition = scrollPosition - workItemWidth;
+            } else {
+                scrollPosition = carouselWidth - visibleCarouselWidth;
+            }
+            $('.carousel-inner').animate({scrollLeft: scrollPosition},600);
+        })
+    })()
+} else {
+    myWorkCarouselEl.classList.add('slide');
+}
+
+
+// End my Works
