18be6d5b8590d34359892918ee33a3b02ebbc75f
Mario <ardamomo3@gmail.com>
2025-05-11 14:57:37 +0700

    improve(admin): projects grid layout

--- src/components/admin/projetcs/tableAdmin/buttonActions.tsx
+++ src/components/admin/projetcs/tableAdmin/buttonActions.tsx
@@ -67,11 +67,12 @@ export function ProjectsTableAdminButtonActions({
       className={[
         "inline-flex items-center text-sm font-medium hover:bg-gray-100 dark:hover:bg-gray-700 p-1.5 dark:hover-bg-gray-800 text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100",
         className,
+        open ? 'open' : '',
       ].join(' ')}
       type="button"
       onClick={toggleOpen}
     >
-      <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+      <svg className="w-5 h-5 rotate-90" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
         <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
       </svg>
     </button>
