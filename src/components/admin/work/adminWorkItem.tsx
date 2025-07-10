dc994d5be8f323012009c5f298e61e9fa26860da
Mario <ardamomo3@gmail.com>
2025-07-10 21:32:01 +0700

    feat: admin work-exp page | custom popover with arrow

--- src/components/admin/work/adminWorkItem.tsx
+++ src/components/admin/work/adminWorkItem.tsx
@@ -6,6 +6,9 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import ApartmentRoundedIcon from '@mui/icons-material/ApartmentRounded';
 import DateRangeRoundedIcon from '@mui/icons-material/DateRangeRounded';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 import Box from "@mui/material/Box";
 import Avatar from "@mui/material/Avatar";
@@ -16,6 +19,10 @@ import Link from "@mui/material/Link";
 import { getMonthName } from "@/lib/date";
 import Divider from "@mui/material/Divider";
 import Typography from "@mui/material/Typography";
+import { CustomPopover } from "../custom/popover/popover";
+import { usePopover } from "../custom/popover/hooks";
+import MenuList from "@mui/material/MenuList";
+import MenuItem from "@mui/material/MenuItem";
 
 type Props = {
   data: IWorkExperience;
@@ -30,6 +37,42 @@ export function AdminWorkItem({
   detailsHref,
   onDelete,
 }: Props) {
+  const menuActions = usePopover();
+
+  const renderMenuActions = () => (
+    <CustomPopover
+      open={menuActions.open}
+      anchorEl={menuActions.anchorEl}
+      onClose={menuActions.onClose}
+      slotProps={{ arrow: { placement: 'right-top' } }}
+    >
+      <MenuList>
+        <li>
+          <MenuItem component={RouterLink} href={detailsHref} onClick={() => menuActions.onClose()}>
+            <VisibilityIcon/>
+            View
+          </MenuItem>
+        </li>
+        <li>
+          <MenuItem component={RouterLink} href={editHref} onClick={menuActions.onClose}>
+            <EditIcon/>
+            Edit
+          </MenuItem>
+        </li>
+
+        <MenuItem
+          onClick={() => {
+            menuActions.onClose();
+            onDelete();
+          }}
+          sx={{ color: 'error.main' }}
+        >
+          <DeleteIcon/>
+          Delete
+        </MenuItem>
+      </MenuList>
+    </CustomPopover>
+  );
 
   const renderBotSide = () => <>
     <Divider sx={{ borderStyle: 'dashed' }}/>
@@ -91,7 +134,7 @@ export function AdminWorkItem({
 
   return <>
     <Card>
-      <IconButton sx={{ position: 'absolute', top: 8, right: 8 }}>
+      <IconButton onClick={menuActions.onOpen} sx={{ position: 'absolute', top: 8, right: 8 }}>
         <MoreVertIcon/>
       </IconButton>
 
@@ -140,5 +183,7 @@ export function AdminWorkItem({
 
       {(!!data.location.length || !!data.description) && renderBotSide()}
     </Card>
+
+    {renderMenuActions()}
   </>;
 }
