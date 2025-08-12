23ff80219200e42c96fc7e207af130ebeefb1ef1
Mario <ardamomo3@gmail.com>
2025-08-12 22:42:40 +0700

    fix: error message on production

--- src/contexts/posthogProvider.tsx
+++ src/contexts/posthogProvider.tsx
@@ -4,11 +4,8 @@ import { useEffect } from "react"
 
 import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
-import { useActiveTemplate } from "@/templates/hooks/useActiveTemplate"
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
-  const { templateName } = useActiveTemplate();
-
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
--- src/templates/registered.ts
+++ src/templates/registered.ts
@@ -10,14 +10,19 @@ export const templates = [
 export type TemplateName = (typeof templates)[number];
 
 export async function getActiveTemplate(): Promise<TemplateName> {
-  const activeTemplateIdx = await RepoProfileData.getOne<number>(ACTIVE_TEMPlATE_STORE_ID, 0);
-  const templateName: TemplateName = templates[activeTemplateIdx] ?? templates[0];
+  let templateName: TemplateName = templates[0];
+  try {
+    const activeTemplateIdx = await RepoProfileData.getOne<number>(ACTIVE_TEMPlATE_STORE_ID, 0);
+    templateName = templates[activeTemplateIdx] ?? templates[0];
+  } catch (error) {
+    templateName = templates[0];
+  }
 
   return templateName;
 }
 
 export function getTemplateTheme(name: TemplateName): IThemeOptions {
-  switch(name) {
+  switch (name) {
     case 'default': return defaultTmplTheme;
     default: return defaultTmplTheme;
   }
